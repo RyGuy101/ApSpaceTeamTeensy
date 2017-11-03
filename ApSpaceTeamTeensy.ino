bc9bbc5d7fc15736f50b495be1755b95b202315f
Ryan N <ryguyn@gmail.com>
2017-11-03 01:48:51 -0700

    Almost done I hope!

--- ApSpaceTeamTeensy.ino
+++ ApSpaceTeamTeensy.ino
@@ -3,30 +3,88 @@
 #include <RF24.h>
 #include <RF24_config.h>
 
-const CE_PIN = 0;//TODO
-const CSN_PIN = 0;//TODO
+#define CE_PIN 0 //TODO
+#define CSN_PIN 0 //TODO
+#define READING_PIPE 0xC2C2C2C2C2
+#define WRITING_PIPE 0xE7E7E7E7E7
+#define PAYLOAD_SIZE 32
 
-const LED_1_PIN = 2;
-const LED_2_PIN = 3;
-const LED_3_PIN = 4;
+#define LED_1_PIN 2
+#define LED_2_PIN 3
+#define LED_3_PIN 4
+
+#define LED_1 0x01
+#define LED_2 0x02
+#define LED_3 0x03
+
+#define CORRECT_BUTTONS 0x04
+#define WRONG_BUTTONS 0x05
+
+uint8_t received_data[PAYLOAD_SIZE];
+
+const uint8_t maxSequenceLength = PAYLOAD_SIZE;
+uint8_t ledSequence[maxSequenceLength];
+uint8_t sequenceIndex = 0;
+bool showNewSequence = true;
 
 RF24 rf24(CE_PIN, CSN_PIN);
 
 void setup() {
   Serial.begin(57600);
   pinMode(LED_1_PIN, OUTPUT);
+  pinMode(LED_2_PIN, OUTPUT);
+  pinMode(LED_3_PIN, OUTPUT);
   rf24.begin();
   rf24.setChannel(13);
   rf24.setPALevel(RF24_PA_MIN);
-  //TODO Check the datasheet for restrictions on the intended receiving and transmitting addresses. (For example, 0xc2c2c2c2c2 and 0xe7e7e7e7e7 are good values to use.) In the setup code, set the writing and reading pipes in both sets of code accordingly. Make sure the reading pipe address of the transmitter matches the writing pipe address of the receiver and vice versa.
+  rf24.openReadingPipe(1, READING_PIPE);
+  rf24.openWritingPipe(WRITING_PIPE);
   rf24.setCRCLength(RF24_CRC_16);
+
+  rf24.setPayloadSize(PAYLOAD_SIZE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  rf24.startListening();
-  //TODO: In the loop, call the available function to check when there are bytes to be read.
-  rf24.stopListening();
-  digitalWrite(LED_1_PIN, HIGH);
-  delay(10);
+  if (showNewSequence) {
+    showNewSequence = false;
+    ledSequence[sequenceIndex] = random(1, 4);
+    flashLedSequence();
+    rf24.write(ledSequence, maxSequenceLength);
+    rf24.startListening();
+    sequenceIndex++;
+  }
+  
+  if(rf24.available()) {
+    rf24.read(&received_data, PAYLOAD_SIZE);
+    rf24.stopListening();
+    if (received_data[0] == CORRECT_BUTTONS) {
+      if (sequenceIndex == maxSequenceLength) {
+        flashWin();
+        sequenceIndex = 0;
+      } else {
+        flashCorrect();
+      }
+    } else {
+      flashWrong();
+      sequenceIndex = 0;
+    }
+    showNewSequence = true;
+  }  
 }
+
+void flashLedSequence() {
+  
+}
+
+void flashCorrect() {
+  
+}
+
+void flashWrong() {
+  
+}
+
+void flashWin() {
+  
+}
+
